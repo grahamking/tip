d3fedc3ddbcaf4166e18975187251ae0bf432d81
Graham King <graham@gkgk.org>
2012-02-12 17:17:23 -0800

    Grouped functions into TimeFile and Timer objects

--- tip.py
+++ tip.py
@@ -6,202 +6,186 @@ import sys
 import os.path
 from datetime import datetime, timedelta
 
-FILENAME = os.path.expanduser('~/.tip')
 FORMAT = '%Y-%m-%d %H:%M'
+FILENAME = os.path.expanduser('~/.tip')
 DAY_HOURS = 8       # Target hours for the day
 
 
-#
-# Commands
-#
+class TimeFile(object):
+    """A file containing time information"""
 
-def on():
-    """Timer start"""
-    if is_timer_on():
-        print('ERROR: Timer is running. Use "tip off" to stop it first.')
-        print('If you forgot to stop it earlier, edit "%s" after' % FILENAME)
-        return
+    def __init__(self, filename):
+        self.filename = filename
 
-    if has_previous_day():
-        archive()
-        print('Archived previous day')
+        self.status = 'OFF'
+        self.file_date = None
+        self.elapsed = timedelta(0)
+        self.notes = []
 
-    record('START %s' % now())
-    print('Timer started')
+        self.parse_file()
 
+    def parse_file(self):
+        """Parse the file and:
+            - calculate elapsed time: self.elapsed
+            - group the notes: self.notes
+            - decided whether timer is running: self.status
+            - date at which timer first started: self.file_date
+        """
 
-def off():
-    """Timer stop"""
-    if not is_timer_on():
-        print('ERROR: Timer is not running. Use "tip on" to start it.')
-        print('If you forgot to start it earlier, edit "%s" after' % FILENAME)
-        return
-
-    record('STOP %s' % now())
-    print('Timer stopped')
-
-
-def note():
-    """Make a note in record file"""
-    content = sys.argv[2]
-    record('NOTE %s' % content)
-    print('Note added')
-
-
-def info():
-    """Show stats"""
-
-    elapsed, notes, status = parse_file()
-
-    print(long_info(elapsed, notes, status))
-
-
-#
-# Internal functions
-#
-
-def record(content):
-    """Write content to record file"""
-    datafile = open(FILENAME, 'at')
-    datafile.write('%s\n' % content)
-    datafile.close()
-
-
-def is_timer_on():
-    """Is the timer currently running"""
-    _, _, status = parse_file()
-    return status == 'ON'
-
-
-def has_previous_day():
-    """Does the file start on a previous day?
-    We assume you never work past midnight. Go to bed already.
-    """
-
-    today = datetime.now()
-    start = file_date()
-    return start.day != today.day or start.month != today.month
-
-
-def file_date():
-    """Date at which the file timer starts."""
-
-    start = None
-    datafile = open(FILENAME, 'rt')
-    for line in datafile:
-        line = line.strip()
-
-        cmd = line.split(' ')[0]
-        if cmd != 'START':
-            continue
-
-        param = ' '.join(line.split(' ')[1:])
-        start = datetime.strptime(param, FORMAT)
-        break
-
-    return start
-
-
-def archive():
-    """Summarise previous day to archive row"""
-
-    elapsed, notes, _ = parse_file()
-    archive_date = file_date().strftime('%Y-%m-%d')
-    record('ARCHIVE %s %s' % (archive_date, short_info(elapsed, notes)))
-
-    keep_only_archive()
-
-
-def keep_only_archive():
-    """Removes all lines from file which aren't archive lines."""
-
-    keep = []
-    read_datafile = open(FILENAME, 'rt')
-    for line in read_datafile:
-        line = line.strip()
-        if line.startswith('ARCHIVE'):
-            keep.append(line)
-    read_datafile.close()
-
-    write_datafile = open(FILENAME, 'wt')
-    write_datafile.write('\n'.join(keep))
-    write_datafile.write('\n')
-    write_datafile.close()
-
-
-def parse_file():
-    """Parse the file and:
-        - calculate elapsed time
-        - group the notes
-        - decided whether timer is running.
-    """
-
-    notes = []
-    elapsed = timedelta(0)
-
-    start = None
-    stop = None
-
-    datafile = open(FILENAME, 'rt')
-    for line in datafile:
-
-        line = line.strip()
-        if not line:
-            continue
-
-        cmd = line.split(' ')[0]
-        param = ' '.join(line.split(' ')[1:])
-        if cmd == 'START':
-            start = datetime.strptime(param, FORMAT)
-
-        elif cmd == 'NOTE':
-            notes.append(param)
-
-        elif cmd == 'STOP':
-            stop = datetime.strptime(param, FORMAT)
-            if start:
-                elapsed += stop - start
-                start = None
-
-    status = 'OFF'
-    # Catch any START without a matching STOP
-    if start:
-        status = 'ON'
-        elapsed += datetime.now() - start
         start = None
+        stop = None
 
-    return elapsed, notes, status
+        datafile = open(self.filename, 'rt')
+        for line in datafile:
+
+            line = line.strip()
+            if not line:
+                continue
+
+            cmd = line.split(' ')[0]
+            param = ' '.join(line.split(' ')[1:])
+            if cmd == 'START':
+                start = datetime.strptime(param, FORMAT)
+                if not self.file_date:
+                    self.file_date = start
+
+            elif cmd == 'NOTE':
+                self.notes.append(param)
+
+            elif cmd == 'STOP':
+                stop = datetime.strptime(param, FORMAT)
+                if start:
+                    self.elapsed += stop - start
+                    start = None
+
+        # Catch any START without a matching STOP
+        if start:
+            self.status = 'ON'
+            self.elapsed += datetime.now() - start
+            start = None
+
+    def short_info(self):
+        """One line summary"""
+        note_str = ', '.join(self.notes)
+        return '%s %s' % (delta_fmt(self.elapsed), note_str)
+
+    def long_info(self):
+        """Detailed multi-line info"""
+
+        detail = []
+        detail.append('Timer is: %s' % self.status)
+
+        remaining = timedelta(hours=DAY_HOURS) - self.elapsed
+        if remaining > timedelta(0):
+            absolute = datetime.now() + remaining
+
+        detail.append('Elapsed:\t%s' % bold(delta_fmt(self.elapsed)))
+
+        if remaining:
+            detail.append('Remaining:\t%s' % delta_fmt(remaining))
+
+            if self.status == 'ON':
+                detail.append('Finish at:\t%s' % absolute.strftime('%H:%M'))
+
+        if self.notes:
+            note_str = ', '.join(self.notes)
+            detail.append('Notes:\t%s' % note_str)
+
+        return '\n'.join(detail)
+
+    def is_timer_on(self):
+        """Is the timer currently running"""
+        return self.status == 'ON'
+
+    def has_previous_day(self):
+        """Does the file start on a previous day?
+        We assume you never work past midnight. Go to bed already.
+        """
+
+        today = datetime.now()
+        start = self.file_date
+        return start.day != today.day or start.month != today.month
 
 
-def short_info(elapsed, notes):
-    """One line summary"""
-    note_str = ', '.join(notes)
-    return '%s %s' % (delta_fmt(elapsed), note_str)
+class Timer(object):
+    """Main object"""
 
+    def __init__(self, filename):
+        self.filename = filename
+        self.time_file = TimeFile(filename)
 
-def long_info(elapsed, notes, status):
-    """Detailed multi-line info"""
+    #
+    # Commands
+    #
 
-    detail = []
-    detail.append('Timer is: %s' % status)
+    def on(self):
+        """Timer start"""
+        if self.time_file.is_timer_on():
+            print('ERROR: Timer is running. Use "tip off" to stop it first.')
+            print('If you forgot to stop it earlier, edit "%s" after' % self.filename)
+            return
 
-    remaining = timedelta(hours=DAY_HOURS) - elapsed
-    if remaining > timedelta(0):
-        absolute = datetime.now() + remaining
+        if self.time_file.has_previous_day():
+            self.archive()
+            print('Archived previous day')
 
-    detail.append('Elapsed:\t%s' % bold(delta_fmt(elapsed)))
+        self.record('START %s' % now())
+        print('Timer started')
 
-    if remaining:
-        detail.append('Remaining:\t%s' % delta_fmt(remaining))
+    def off(self):
+        """Timer stop"""
+        if not self.time_file.is_timer_on():
+            print('ERROR: Timer is not running. Use "tip on" to start it.')
+            print('If you forgot to start it earlier, edit "%s" after' % self.filename)
+            return
 
-        if status == 'ON':
-            detail.append('Finish at:\t%s' % absolute.strftime('%H:%M'))
+        self.record('STOP %s' % now())
+        print('Timer stopped')
 
-    if notes:
-        note_str = ', '.join(notes)
-        detail.append('Notes:\t%s' % note_str)
+    def note(self):
+        """Make a note in record file"""
+        content = sys.argv[2]
+        self.record('NOTE %s' % content)
+        print('Note added')
 
-    return '\n'.join(detail)
+    def info(self):
+        """Show stats"""
+        print(self.time_file.long_info())
+
+    #
+    # Internal functions
+    #
+
+    def record(self, content):
+        """Write content to record file"""
+        datafile = open(self.filename, 'at')
+        datafile.write('%s\n' % content)
+        datafile.close()
+
+    def archive(self):
+        """Summarise previous day to archive row"""
+
+        archive_date = self.time_file.file_date.strftime('%Y-%m-%d')
+        self.record('ARCHIVE %s %s' % (archive_date,
+                                       self.time_file.short_info()))
+
+        self.keep_only_archive()
+
+    def keep_only_archive(self):
+        """Removes all lines from file which aren't archive lines."""
+
+        keep = []
+        read_datafile = open(self.filename, 'rt')
+        for line in read_datafile:
+            line = line.strip()
+            if line.startswith('ARCHIVE'):
+                keep.append(line)
+        read_datafile.close()
+
+        write_datafile = open(self.filename, 'wt')
+        write_datafile.write('\n'.join(keep))
+        write_datafile.write('\n')
+        write_datafile.close()
 
 
 def now():
@@ -238,23 +222,15 @@ def main(argv=None):
     if not argv:
         argv = sys.argv
 
-    cmds = {
-        'on': on,
-        'start': on,
+    cmds = ['on', 'off', 'note', 'info']
 
-        'off': off,
-        'stop': off,
-
-        'note': note,
-
-        'info': info,
-        'status': info}
-
+    timer = Timer(FILENAME)
     if len(argv) >= 2 and argv[1] in cmds:
-        cmds[argv[1]]()
+        func = getattr(timer, argv[1])
+        func()
     else:
         print('tip [on|off|note] [note content]\n')
-        info()
+        timer.info()
 
 
 if __name__ == '__main__':
